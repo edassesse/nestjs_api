75a28460a63e651a797ba92f9ba607345c38c053
Ernest Dassesse <edassess@ernests-macbook-pro.home>
2023-08-17 16:21:11 +0200

    Sending emails via sendgrid

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { UserCredentialsDto } from "./dto/user-credentials.dto";
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserCredentialsDto } from './dto/user-credentials.dto';
 import * as bcrypt from 'bcrypt';
-import { User } from "./user.entity";
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
@@ -21,5 +21,23 @@ export class UsersService {
     } catch (error) {
       throw new InternalServerErrorException();
     }
+    console.log(process.env.SENDGRID_API_KEY);
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: userCredentialsDto.email, // Change to your recipient
+      from: 'dassesseernest@gmail.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
